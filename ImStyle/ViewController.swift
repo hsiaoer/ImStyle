fcd70045cefb4c0b7d42e0f85fe0d98f89d41515
Jake Carlson <jrcarlson@smu.edu>
2017-10-03 16:28:30 -0500

    Empty single view app

---
+++ ImStyle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImStyle
+//
+//  Created by Jake Carlson on 10/3/17.
+//  Copyright © 2017 ImageStyle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
