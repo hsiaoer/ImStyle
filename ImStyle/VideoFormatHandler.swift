31d6e403f4fd48da035dc113a3120ec97c94b36b
Jake Carlson <jrcarlson@smu.edu>
2017-10-25 16:57:16 -0500

    Generates frames with copyCGImage

--- ImStyle/VideoFormatHandler.swift
+++ ImStyle/VideoFormatHandler.swift
@@ -20,7 +20,7 @@ class VideoFormatHandler {
     var callback: Selector
     
     init(mainVC: MainViewController, imageSize: Int, callback: Selector) {
-        self.sampleRate = 0.05
+        self.sampleRate = 0.01
         self.mainVC = mainVC
         self.numFramesLoaded = 0
         self.numFramesRequested = 0
@@ -31,12 +31,37 @@ class VideoFormatHandler {
     func videoToArray(videoResource: NSURL) {
         self.numFramesLoaded = 0
         let asset = AVURLAsset(url: videoResource as URL)
+        DispatchQueue.global().async {
+            self.sampleFrames(asset: asset)
+        }
+//        let generator = AVAssetImageGenerator.init(asset: asset)
+//        let times = Array(stride(from: 0.0, to: Double(CMTimeGetSeconds(asset.duration)), by: self.sampleRate))
+//        self.numFramesRequested = times.count
+//        generator.generateCGImagesAsynchronously(forTimes: times as [NSValue], completionHandler: self.frameGenCompletion(reqT:img:actualT:result:err:))
+    }
+    
+    func sampleFrames(asset: AVAsset) {
         let generator = AVAssetImageGenerator.init(asset: asset)
         let times = Array(stride(from: 0.0, to: Double(CMTimeGetSeconds(asset.duration)), by: self.sampleRate))
         self.numFramesRequested = times.count
-        print(Double(asset.duration.value))
-        print(self.numFramesRequested)
-        generator.generateCGImagesAsynchronously(forTimes: times as [NSValue], completionHandler: self.frameGenCompletion(reqT:img:actualT:result:err:))
+        for t in times {
+            do {
+                let image = try generator.copyCGImage(at: CMTimeMakeWithSeconds(t, asset.duration.timescale), actualTime: nil)
+                
+                let frame = UIImage(cgImage: image).scaled(to: CGSize(width: self.imageSize, height: self.imageSize), scalingMode: .aspectFit)
+                self.mainVC.videoFrames[0].append(frame)
+                self.numFramesLoaded += 1
+            }
+            catch {
+                self.numFramesRequested -= 1
+            }
+        }
+        DispatchQueue.main.async {
+            self.mainVC.videoTimer?.invalidate()
+            self.mainVC.videoTimer = Timer.scheduledTimer(timeInterval: self.sampleRate, target: self.mainVC, selector: self.callback, userInfo: nil, repeats: true)
+            self.mainVC.displayingVideo = true
+            self.mainVC.updateStyle(oldStyle: 0)
+        }
     }
     
     func frameGenCompletion(reqT: CMTime, img: CGImage?, actualT: CMTime, result: AVAssetImageGeneratorResult, err: Error?) -> Void {
